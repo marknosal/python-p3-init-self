15d5cf3b218a57ba120f91a3b27868dbac119832
Mark Nosal <mark.c.nosal@gmail.com>
2023-06-03 19:05:01 -0500

    finished lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt', favorite_toy='Anything'):
+        self.name = name
+        self.breed = breed
+        self.favorite_toy = favorite_toy
+        print(f'{name} has been born!')
+    def bark(self):
+        print("Woof!")
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+        print(f'{self.name} has been adopted by {owner_name}!')
+
+
+fido = Dog("Fido")
